527a65fbd18e7eddf48b6ffcd8f21d279f93800a
Gyorgy Matyas <gyorgy.matyi@gmail.com>
2025-03-13 00:33:59 +0200

    lab2 init

---
+++ lab1_shell.nix
@@ -0,0 +1,29 @@
+let
+  # https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      tkinter
+      pillow
+      pybind11
+      setuptools
+    ]))
+    pkgs.gcc
+  ];
+
+  shellHook = ''
+    export CPLUS_INCLUDE_PATH=$(python -c "import pybind11; print(pybind11.get_include())"):$CPLUS_INCLUDE_PATH
+    echo "Pybind11 include path set: $CPLUS_INCLUDE_PATH"
+    
+    if [ ! -f "build/lib.*-*/ciff.*.so" ]; then
+      echo "Building and installing CIFF package..."
+      python setup.py build
+      python setup.py install --user
+      export PYTHONPATH=~/.local/lib/python3.11/site-packages/ciff-0.1-py3.11-linux-x86_64.egg:$PYTHONPATH
+      echo "CIFF package installed successfully!"
+    else
+      echo "CIFF package already built."
+    fi
+  '';
+}
